a9453e600b548358c3fb912a6c0db9101cc860ff
WooilJeong <wooil@kakao.com>
2022-08-16 13:53:59 +0900

    fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,9 +12,14 @@
 #
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath("../.."))
 from PyKakao import __project__, __version__, __author__, __contact__, __github__
 
+import sphinx_adc_theme
+
+html_theme = "sphinx_adc_theme"
+html_theme_path = [sphinx_adc_theme.get_html_theme_path()]
 
 # -- Project information -----------------------------------------------------
 
